64a5f550e7e922a470e078a7bdb466e207b8e07e
Yeiner CQ <ycarvajalq@gmail.com>
2022-05-19 21:13:25 -0500

    add participants delete reference

--- app/models/task.rb
+++ app/models/task.rb
@@ -15,7 +15,7 @@ class Task < ApplicationRecord
   belongs_to :category
   belongs_to :owner, class_name: 'User'
   has_many :participating_users, class_name: 'Participant'
-  has_many :participants, through: :participating_users, source: :user
+  has_many :participants, through: :participating_users, source: :user, dependent: :destroy
 
   validates :participating_users, presence: true
   validates :name, :description, presence: true
